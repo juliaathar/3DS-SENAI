6c46490f2bf44d5110e983bd9f401c2c84de2cc6
juliaathar <juathar@gmail.com>
2024-03-11 10:36:30 -0300

    :sparkles: Feito rota entre dois pontos
    Feito com dependencia react native maps directions

--- mapsExample/App.js
+++ mapsExample/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 // Importando dependencia do mapa
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import {
   //solicita o acesso a localizacao
   requestForegroundPermissionsAsync,
@@ -11,6 +11,9 @@ import {
   getCurrentPositionAsync
 } from "expo-location"
 import { useEffect, useState } from 'react';
+
+import MapViewDirections from 'react-native-maps-directions';
+import { mapskey } from './utils/apiKey';
 
 export default function App() {
 
@@ -42,15 +45,39 @@ export default function App() {
                 latitudeDelta: 0.005,
                 longitudeDelta: 0.005
               }}
+              provider={PROVIDER_GOOGLE}
+              customMapStyle={grayMapStyle}
               style={styles.map}
             >
-              <Marker 
+              <Marker
                 coordinate={{
                   latitude: initialPosition.coords.latitude,
                   longitude: initialPosition.coords.longitude,
                 }}
                 title='Initial position'
                 pinColor={"red"}
+              />
+
+              <MapViewDirections
+                origin={initialPosition.coords}
+                destination={{
+                  latitude: -9.5260,
+                  longitude: -43.6041,
+                  latitudeDelta: 0.005,
+                  longitudeDelta: 0.005
+                }}
+                strokeWidth={5}
+                strokeColor='#496bba'
+                apikey={mapskey}
+              />
+
+              <Marker
+                coordinate={{
+                  latitude: -9.5260,
+                  longitude: -43.6041,
+                }}
+                title='Initial position'
+                pinColor={"blue"}
               />
             </MapView>
           ) : (
@@ -76,3 +103,226 @@ const styles = StyleSheet.create({
     width: "100%"
   }
 });
+
+const grayMapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#E1E0E7",
+      },
+    ],
+  },
+  {
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        saturation: -5,
+      },
+      {
+        lightness: -5,
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#33303E",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#66DA9F",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1B1B1B",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#C6C5CE",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ACABB7",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8EA5D9",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+];
